ed3e382da4e3d6b712a1f39bede22b320a86d14a
Jeremy Bastardie <jeremy.bastardie@gmail.com>
2016-04-14 11:16:51 +0200

    or ynn

--- ConvertisseurApp/src/convertisseurapp/ConvertisseurApp.java
+++ ConvertisseurApp/src/convertisseurapp/ConvertisseurApp.java
@@ -11,6 +11,9 @@ public class ConvertisseurApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ConvertisseurFrame my_frame = new ConvertisseurFrame();
+        //ConvertisseurFrame my_frame = new ConvertisseurFrame();
+        InterfaceConvertisseur model = new ModelConvertisseur();
+        Interf view = new UIConvertisseur();
+        ControllerConvertisseur controller = new ControllerConvertisseur(model, view);
     }    
 }
